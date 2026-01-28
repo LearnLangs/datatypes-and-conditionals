c7de0188cda8eb37ff29f9c1bc920fe2460ab39e
Davisone <67480359+krush-ghod@users.noreply.github.com>
2026-01-27 22:58:54 -0500

    this is a progressive journey

--- python/types.py
+++ python/types.py
@@ -22,6 +22,32 @@
             there are several explanation that can be given to this data type. 
             thus the sequence type mainly keep data in the sequencial order.
             there are three sequence types in python which may vary in other languages
-            1. List -: As the name denotes, it is a list or set of data which may include any of the three types above
+                1. List -: As the name denotes, it is a list or set of data which may include any of the three types above
+                    list are kind of data that can be altered even after declaration. it is started with a square bracket and have it values separated by commas
+                2. Turple -: Unlike lists, turples are not flexible in terms of altering it. once it is saved you can not change any of it values.
+                    it is started with parenthesis and comma separating it values
+                3. Range -: I would say this is a function in python that assigns a set of numbers mostly integers to a variable
+                    in a sequencial order. ranges can be called using the it name and passing an integer as an argument.
+        
+        5. Mapping type:
+            consider this data type to be just a key value pair of information stored in a variable.
+            in other languages, its a bit complicated to get a dict. but in python, you can simple create a dict by calling the dict function
+            or creating a variable and assigning either an empty curly bracies to it. a language like JS would consider it dicts mostly as objects.
+            
+        6. Set type:
+            consider this type of the data as the normal set of infomation stored in a curly bracket, thus the set type does not have
+            a key-value pair data, it is either saving a string or numbers, there are two types of set data type
+                1. Set -: this is just the normal set you know, the one that contains only the values without keys
+                2. Froven set -: to identify a froven set, it is a normal set that is further rapped in parenthesis
+        
+        7. Binary type:
+            this kind of data type deals with things like images, files, and so on. the computer processes this as the binary numbers
+            as a tweak of this images and files are made of either 0 and 1 or is in the hex form.
+            we have:
+                1. Bytes -: an immutable sequence of integers
+                2. Bytearrays -: a mutable sequence of integers
+                3. Memoryview -: this is a built in python function that allows you to access the memory allocation of an object
+        8. None type:
+            just nothing.
 """
 
